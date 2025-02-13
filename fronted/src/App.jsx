656c751130705a62f4c74cac6ac359c7997c83d0
Albstrow <95503085+Albstrow@users.noreply.github.com>
2025-02-13 16:37:27 +0530

    Update App.jsx

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductList from "./components/ProductList";
+import AdminRedirect from "./components/AdminRedirect";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ProductList />} />
+        <Route path="/admin" element={<AdminRedirect />} />
       </Routes>
     </Router>
   );
