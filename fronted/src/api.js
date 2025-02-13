e76ed98de1f402036248238bfd04fd701c9d08dd
Albstrow <95503085+Albstrow@users.noreply.github.com>
2025-02-13 16:12:21 +0530

    Update api.js

--- fronted/src/api.js
+++ fronted/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/api/products/";
+const API_URL = "https://vishalecom.pythonanywhere.com/api/products/";
 
 export const getProducts = async () => {
   const response = await axios.get(API_URL);
